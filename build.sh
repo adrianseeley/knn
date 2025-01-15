21a8f66d47b806eb35722121af650a7a279875be
adrianseeley <contact@adrianseeley.com>
2025-01-15 17:16:45 -0500

    results

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-clang knn.c -o knn.exe -O3 -march=native
+clang knn_k_dt_de_reciprocal.c -o knn_k_dt_de_reciprocal.exe -O3 -march=native
+clang knn_k_dt_de_reciprocal_rooted.c -o knn_k_dt_de_reciprocal_rooted.exe -O3 -march=native
+clang knn_k_dt_de_average.c -o knn_k_dt_de_average.exe -O3 -march=native
+clang knn_k_dt_de_linear.c -o knn_k_dt_de_linear.exe -O3 -march=native
+clang knn_k_dt_de_linear_rooted.c -o knn_k_dt_de_linear_rooted.exe -O3 -march=native

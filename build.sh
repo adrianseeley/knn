5cb9b410ece02fb8bd8c535b51fff07b1cc86a63
adrianseeley <contact@adrianseeley.com>
2025-01-16 10:45:22 -0500

    phase 0

--- build.sh
+++
@@ -1,5 +0,0 @@
-clang knn_k_dt_de_reciprocal.c -o knn_k_dt_de_reciprocal.exe -O3 -march=native
-clang knn_k_dt_de_reciprocal_rooted.c -o knn_k_dt_de_reciprocal_rooted.exe -O3 -march=native
-clang knn_k_dt_de_average.c -o knn_k_dt_de_average.exe -O3 -march=native
-clang knn_k_dt_de_linear.c -o knn_k_dt_de_linear.exe -O3 -march=native
-clang knn_k_dt_de_linear_rooted.c -o knn_k_dt_de_linear_rooted.exe -O3 -march=native
---
+++ phase0/build.sh
@@ -0,0 +1,5 @@
+clang knn_k_dt_de_reciprocal.c -o knn_k_dt_de_reciprocal.exe -O3 -march=native
+clang knn_k_dt_de_reciprocal_rooted.c -o knn_k_dt_de_reciprocal_rooted.exe -O3 -march=native
+clang knn_k_dt_de_average.c -o knn_k_dt_de_average.exe -O3 -march=native
+clang knn_k_dt_de_linear.c -o knn_k_dt_de_linear.exe -O3 -march=native
+clang knn_k_dt_de_linear_rooted.c -o knn_k_dt_de_linear_rooted.exe -O3 -march=native
